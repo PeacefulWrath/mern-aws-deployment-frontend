e47124b062fd7d195735a12e80b361d3d5c68a1e
srinjay kapri <srinjayayan1262000@gmail.com>
2024-04-06 21:55:22 +0530

    dockerize

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    if(process.env.REACT_APP_NODE_ENV==="production"){
+    fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/users`)
+      .then((data) => {
+        return data.json();
+      })
+      .then((dbUsers) => {
+        console.log(dbUsers.users);
+        setUsers([...dbUsers.users]);
+      });
+    }
+  }, []);
+
   return (
-    <div className="App">
-       <h1>mern stack awes deployment</h1>
-    </div>
+    <>
+      <h1>mern stack aws deployment</h1>
+      {users.length!==0 && users.map((u) => (
+        <div>
+          <p>{u?.user_name}</p>
+          <p>{u?.user_email}</p>
+          <p>{u?.user_contact}</p>
+          <hr/>
+        </div>
+      ))}
+    </>
   );
 }
 
